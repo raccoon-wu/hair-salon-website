3c318dc043a03ccddfcfd260d8a4f16a17a43502
Amy <amywu177@gmail.com>
2024-09-30 23:17:55 +1000

    Motion-landing

--- src/Components/Landing.tsx
+++ src/Components/Landing.tsx
@@ -1,66 +1,92 @@
-"use client"; 
+"use client";
 import React, { useState } from 'react';
 import FPDisplay from '../app/Assets/Images/After.png';
 import { FaGoogle, FaInstagram, FaFacebook } from "react-icons/fa6";
+import { easeIn, motion } from 'framer-motion';
 
 export default function LandingPage() {
 
   const landingIcons = {
-    size: "2em", 
+    size: "2em",
     className: "ml-1 h-full max-h-16 ml-0 mr-0 md:ml-5 md:mr-5 cursor-pointer rounded-2xl hover:bg-zinc-700 hover:w-20 hover:h-16 hover:p-3 duration-300",
     style: {
-      color: '#FCE295', 
+      color: '#FCE295',
       minWidth: "1.8em",  // Set minimum width to 1.8em
       minHeight: "1.8em"  // Set minimum height to 1.8em
     }
   };
 
-
   const landingHeading = {
-    className: "cursor-default text-4xl xl:text-5xl shorter-screen:2xl:text-5xl taller-screen:2xl:text-6xl text-center uppercase mb-3 min-w-fit whitespace-nowrap duration-500"
+    className: "cursor-default text-4xl xl:text-5xl shorter-screen:2xl:text-5xl taller-screen:2xl:text-6xl text-center uppercase mb-3 min-w-fit whitespace-nowrap duration-500",
   };
 
-    return (
-      <>
+  const landingHeadingVariant = {
+      hidden: { opacity: 0 },
+      show: {
+        opacity: 1,
+        transition: {
+          delayChildren: 1.5, // Delay before starting the animation
+          staggerChildren: 0.7, // Stagger children after the delay
+          duration: 2,
+        }
+      }
+  }
+
+  const headingMotion = {
+    hidden: { opacity: 0, x: -100},
+    show: {
+      opacity: 1,
+      x:0,
+      transition: {
+        duration: 0.5, // Duration of the fade-in for each child
+        ease: "circIn"
+      },
+    },
+  }
+
+  return (
+    <>
       <div className="flex flex-row">
 
         <div className="flex w-1/2 h-[calc(100vh-5rem)] min-h-[600px] justify-center items-center">
 
-            <div className="flex flex-col w-1/2 ml-30 justify-center items-center">
+          <motion.div
+            variants={landingHeadingVariant}
+            initial="hidden"
+            animate="show"
+            className="flex flex-col w-1/2 ml-30 justify-center items-center">
 
-                <h1 {...landingHeading}>Your hair,</h1>
-                <h1 {...landingHeading}>our passion</h1>
-                <p className="cursor-default text-base lg:text-xl text-center text-white max-w-md mt-2 duration-500">With over 20 years of experience, our salon offers expert, personalized haircare at an affordable price. Enjoy top-quality service every time, where luxury meets your budget.</p>
+            <motion.h1 className={landingHeading.className} 
+            variants={headingMotion}>
+              Your hair,
+            </motion.h1>
 
-                    <div className='flex flex-row justify-evenly items-center h-10 w-full xl:w-3/5 mt-7'>
-                    {/* {...xyz }is a spread operator. It's useful for copying components like:
-                    const obj1 = { a: 1, b: 2 };
-                    const obj2 = { ...obj1, c: 3 };
-                    console.log(obj2); // { a: 1, b: 2, c: 3 }
-                    
-                    Or it's useful for updating parts of objects without mutating the original like:
-                    const [user, setUser] = useState({ name: 'John', age: 25 });
+            <motion.h1 className={landingHeading.className} 
+            variants={headingMotion}>
+              our passion
+            </motion.h1>
 
-                    // Only update the age without mutating the entire object
-                    setUser((prevUser) => ({ ...prevUser, age: 26 }));
+            <motion.p className="cursor-default text-base lg:text-xl text-center text-white max-w-md mt-2 duration-500" 
+            variants={headingMotion}>
+              With over 20 years of experience, our salon offers expert, personalized haircare at an affordable price. Enjoy top-quality service every time, where luxury meets your budget.
+            </motion.p>
 
-                    // Now user is { name: 'John', age: 26 }
-                    
-                    */}
+            <motion.div className='flex flex-row justify-evenly items-center h-10 w-full xl:w-3/5 mt-7' variants={headingMotion}>
 
-                        <a href="https://g.co/kgs/i3gjw9z" target="_blank" rel="noopener noreferrer">
-                            <FaGoogle {...landingIcons}/>
-                        </a>
+              <a href="https://g.co/kgs/i3gjw9z" target="_blank" rel="noopener noreferrer">
+                <FaGoogle {...landingIcons} />
+              </a>
 
-                        <a href="https://www.facebook.com/HYGforesthill" target="_blank" rel="noopener noreferrer">
-                            <FaFacebook {...landingIcons}/>
-                        </a>
+              <a href="https://www.facebook.com/HYGforesthill" target="_blank" rel="noopener noreferrer">
+                <FaFacebook {...landingIcons} />
+              </a>
 
-                        <a href="https://www.instagram.com/hygforesthill?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">
-                            <FaInstagram {...landingIcons}/>
-                        </a>
-                    </div>
-            </div>
+              <a href="https://www.instagram.com/hygforesthill?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">
+                <FaInstagram {...landingIcons} />
+              </a>
+            </motion.div>
+
+          </motion.div>
 
         </div>
         {/* 
@@ -71,11 +97,27 @@ export default function LandingPage() {
         xl: 1280px
         2xl: 1536px
          */}
-            <div className="w-1/2 h-fulloverflow-hidden">
-              <img src={FPDisplay.src} className='h-full w-full object-cover object-left'/>
-                {/* <img src={FPDisplay.src} className='absolute h-full scale-[1.5] right-0 top-[50px] translate-x-[250px] xl:translate-x-[100px] 2xl:-translate-x-[100px] object-cover'/> */}
-            </div>
+        <div className="w-1/2 h-fulloverflow-hidden">
+          <img src={FPDisplay.src} className='h-full w-full object-cover object-left' />
+          {/* <img src={FPDisplay.src} className='absolute h-full scale-[1.5] right-0 top-[50px] translate-x-[250px] xl:translate-x-[100px] 2xl:-translate-x-[100px] object-cover'/> */}
+        </div>
       </div>
-      </>
-    );
-  }
+    </>
+  );
+}
+
+
+{/* {...xyz }is a spread operator. It's useful for copying components like:
+        const obj1 = { a: 1, b: 2 };
+        const obj2 = { ...obj1, c: 3 };
+        console.log(obj2); // { a: 1, b: 2, c: 3 }
+        
+        Or it's useful for updating parts of objects without mutating the original like:
+        const [user, setUser] = useState({ name: 'John', age: 25 });
+
+        // Only update the age without mutating the entire object
+        setUser((prevUser) => ({ ...prevUser, age: 26 }));
+
+        // Now user is { name: 'John', age: 26 }
+        
+        */}
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,5 +1,7 @@
+"use client"; 
 import "../app/globals.css";
 import { RiScissorsFill } from "react-icons/ri";
+import { motion } from 'framer-motion';
 
 export default function Navbar() {
 
@@ -9,23 +11,45 @@ export default function Navbar() {
 
     return (
       <>
-      <div className="flex shorter-screen:h-20 taller-screen:h-24 bg-nav-dark-gray items-center">
-          <div className=" ml-10 flex flex-row h-full
-                          w-1/3 xl:w-1/4 min-w-fit items-center">
-              <h2 className="shorter-screen:text-4xl taller-screen:text-5xl font-bold mt-5 bg-gradient-to-r from-dark-gold to-lighter-gold bg-clip-text text-transparent cursor-default"> HAIR YOU GO </h2>
-              <RiScissorsFill size="2.3em" className="ml-1 h-full text-lighter-gold mt-5 taller-screen:ml-2" style = {{transform: 'rotate(90deg)' }}/>
-          </div>
-          <div className=" m-10 xl:m-20
-                          w-2/3 xl:w-3/4 ">
-              <ul className="flex flex-row justify-end space-x-10 md:space-x-20 lg:space-x-30 xl:space-x-36 pt-5 text-zinc-300 
-              text-md lg:shorter-screen:text-xl lg:taller-screen:text-2xl">
-                <li {...navButtonStyle}>About</li>
-                <li {...navButtonStyle}>Gallery & Services</li>
-                <li {...navButtonStyle}>Contact</li>
-              </ul>
-          </div>
+      <div className="flex flex-col overflow-x-hidden">
+          <motion.div 
+          variants={{
+            hidden:  {opacity: 0}, 
+            show:    {opacity: 1, transition: {duration: 2,}},            
+          }}
+          initial="hidden"
+          animate="show"
+          className="flex shorter-screen:h-20 taller-screen:h-24 bg-black items-center">
+              <div className=" ml-10 flex flex-row h-full
+                              w-1/3 xl:w-1/4 min-w-fit items-center">
+                  <h2 className="shorter-screen:text-4xl taller-screen:text-5xl font-bold mt-5 bg-gradient-to-r from-dark-gold to-lighter-gold bg-clip-text text-transparent cursor-default"> HAIR YOU GO </h2>
+                  <RiScissorsFill size="2.3em" className="ml-1 h-full text-lighter-gold mt-5 taller-screen:ml-2" style = {{transform: 'rotate(90deg)' }}/>
+              </div>
+              <div className=" m-10 xl:m-20
+                              w-2/3 xl:w-3/4 ">
+                  <ul className="flex flex-row justify-end space-x-10 md:space-x-20 lg:space-x-30 xl:space-x-36 pt-5 text-zinc-300 
+                  text-md lg:shorter-screen:text-xl lg:taller-screen:text-2xl">
+                    <li {...navButtonStyle}>About</li>
+                    <li {...navButtonStyle}>Gallery & Services</li>
+                    <li {...navButtonStyle}>Contact</li>
+                  </ul>
+              </div>
+          </motion.div>
+          <motion.div 
+          variants={{
+            hidden:  {opacity: 0}, 
+            show: {
+              opacity: 1,
+              transition: {
+                delay: 0.4,
+                duration: 1,
+              }
+            }                        
+          }} 
+          initial="hidden"
+          animate="show"
+          className="self-center h-1.5 w-[calc(100vw-6rem)] ml-10 mr-10 bg-dark-gold"></motion.div>
       </div>
-      <div className="self-center h-1.5 w-[calc(100vw-6rem)] ml-10 mr-10 bg-dark-gold"></div>
       </>
     );
   }
